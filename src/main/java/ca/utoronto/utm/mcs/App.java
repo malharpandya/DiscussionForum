afc438550bfd9ab5fa65380b5e037af175178b74
Malhar Pandya <malharpandya@MacBook-Pro.local>
2020-11-05 13:15:38 -0500

    finished testing, removed unused imports, formatted all code

--- src/main/java/ca/utoronto/utm/mcs/App.java
+++ src/main/java/ca/utoronto/utm/mcs/App.java
@@ -1,26 +1,17 @@
 package ca.utoronto.utm.mcs;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import com.sun.net.httpserver.HttpServer;
 
-import javax.inject.Inject;
+public class App {
+	static int port = 8080;
 
+	public static void main(String[] args) throws IOException {
+		Dagger service = DaggerDaggerComponent.create().buildMongoHttp();
 
-public class App
-{
-    static int port = 8080;
+		service.getServer().createContext("/api/v1/post", new Post(service.getClient()));
 
-    public static void main(String[] args) throws IOException
-    {
-    	Dagger service = DaggerDaggerComponent.create().buildMongoHttp();
-    	
-    	//Create your server context here
-    	service.getServer().createContext("/api/v1/post", new Post(service.getClient()));
-    	
-    	service.getServer().start();
-    	
-    	System.out.printf("Server started on port %d", port);
-    }
+		service.getServer().start();
+
+		System.out.printf("Server started on port %d", port);
+	}
 }
